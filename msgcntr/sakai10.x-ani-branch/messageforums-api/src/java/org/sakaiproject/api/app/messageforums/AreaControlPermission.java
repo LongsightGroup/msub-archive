63926a5fe2dbc1676fc25b1f5343a8e02b2bdd0d
Sam Ottenhoff <ottenhoff@longsight.com>
2015-09-11 18:28:55 +0000

    forking 10.x msgcntr

---
+++ msgcntr/sakai10.x-ani-branch/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/AreaControlPermission.java
@@ -0,0 +1,57 @@
+/**********************************************************************************
+ * $URL: https://svn.rsmart.com/svn/vendor/branches/sakai/rsmart-cle/msgcntr/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/AreaControlPermission.java $
+ * $Id: AreaControlPermission.java 13692 2009-04-24 16:14:32Z jbush $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+package org.sakaiproject.api.app.messageforums;
+
+public interface AreaControlPermission {
+
+    public Boolean getPostToGradebook();
+    
+    public void setPostToGradebook(Boolean postToGradebook);
+    
+    public Boolean getChangeSettings();
+
+    public void setChangeSettings(Boolean changeSettings);
+
+    public Boolean getMovePostings();
+
+    public void setMovePostings(Boolean movePostings);
+
+    public Boolean getNewForum();
+
+    public void setNewForum(Boolean newForum);
+
+    public Boolean getNewResponse();
+
+    public void setNewResponse(Boolean newResponse);
+
+    public Boolean getNewTopic();
+
+    public void setNewTopic(Boolean newTopic);
+
+    public Boolean getResponseToResponse();
+
+    public void setResponseToResponse(Boolean responseToResponse);
+
+    public String getRole();
+
+    public void setRole(String role);
+
+}
---
+++ msgcntr/sakai10.x-ani-branch/messageforums-app/src/java/org/sakaiproject/tool/messageforums/jsf/HideDivisionComponent.java
@@ -0,0 +1,47 @@
+/**********************************************************************************
+ * $URL: https://svn.rsmart.com/svn/vendor/branches/sakai/rsmart-cle/msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/jsf/HideDivisionComponent.java $
+ * $Id: HideDivisionComponent.java 13692 2009-04-24 16:14:32Z jbush $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+package org.sakaiproject.tool.messageforums.jsf;
+
+import javax.faces.component.UIComponentBase;
+
+/**
+ * @author Chen Wen
+ * @version $Id: HideDivisionComponent.java 13692 2009-04-24 16:14:32Z jbush $
+ * 
+ */
+public class HideDivisionComponent extends UIComponentBase
+{
+  public HideDivisionComponent()
+	{
+		super();
+		this.setRendererType("org.sakaiproject.HideDivision");
+	}
+
+	public String getFamily()
+	{
+		return "HideDivision";
+	}
+	
+	public boolean getRendersChildren()
+	{
+	  return true;
+	}	
+}
