832845d83148784da066d7900db65edc63a3fe7f
Matthew Jones <jonespm@gmail.com>
2015-12-22 21:36:14 +0000

    --- Merging r317044 through r322192 into '.':

--- custom_builds/7/assignment/assignment-10.x/assignment-api/api/src/java/org/sakaiproject/assignment/api/AssignmentService.java
+++ custom_builds/7/assignment/assignment-10.x/assignment-api/api/src/java/org/sakaiproject/assignment/api/AssignmentService.java
@@ -862,9 +862,14 @@ public interface AssignmentService extends EntityProducer
 	public boolean getAllowGroupAssignmentsInGradebook();
 	
 	/**
+	 * Whether a specific user can submit
+	 */
+	public boolean canSubmit(String context, Assignment a, String userId);
+	/**
 	 * Whether the current user can submit
 	 */
 	public boolean canSubmit(String context, Assignment a);
+
 	
 	public Collection<Group> getSubmitterGroupList(String searchFilterOnly, String allOrOneGroup, String searchString, String aRef, String contextString);
 	/**
@@ -882,6 +887,17 @@ public interface AssignmentService extends EntityProducer
 	 * @return
 	 */
 	public List<String> getSubmitterIdList(String searchFilterOnly, String allOrOneGroup, String search, String aRef, String contextString);	
+
+	/**
+	 * Alternative to getSubmittedIdList which returns full user and submissions, since submitterIdList retrieves them anyway
+	 * @param searchFilterOnly If true, return only those ids that matches the group filter and search criteria
+	 * @param allOrOneGroup "all" or specific group reference
+	 * @param aRef Assignment Reference
+	 * @param search The search string
+	 * @param contextString Site id
+	 * @return
+	 */
+	public Map<User, AssignmentSubmission> getSubmitterMap(String searchFilterOnly, String allOrOneGroup, String search, String aRef, String contextString);
 
 	public String escapeInvalidCharsEntry(String accentedString);
 	
