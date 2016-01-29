f01ff83d0700c3fe8967681cc84c165ea718d5a3
Earle Nietzel <enietzel@anisakai.com>
2016-01-29 04:27:09 +0000

    Add availability to drag n drop

--- custom_builds/81/content-10.x_custom/content-tool/tool/src/java/org/sakaiproject/content/tool/ResourcesHelperAction.java
+++ custom_builds/81/content-10.x_custom/content-tool/tool/src/java/org/sakaiproject/content/tool/ResourcesHelperAction.java
@@ -771,6 +771,7 @@ public class ResourcesHelperAction extends VelocityPortletPaneledAction
 		if(ContentHostingService.isAvailabilityEnabled())
 		{
 			context.put("availability_is_enabled", Boolean.TRUE);
+			context.put("upload_visibility_hidden", ServerConfigurationService.getBoolean("content.dnd.visibilty.hidden", false));
 		}
 		
 		if(model.isDropbox)
@@ -1932,7 +1933,8 @@ public class ResourcesHelperAction extends VelocityPortletPaneledAction
 	{
 		String fullPath = request.getParameter("fullPath");
 		String action = request.getParameter("sakai_action");
-		logger.debug("Received action: "+action+" for file: "+fullPath);
+		boolean hidden = Boolean.valueOf(request.getParameter("hidden"));
+		logger.debug("Received action: "+action+" for file: "+fullPath+" with visibilty "+Boolean.toString(!hidden));
 		
 		// set up rundata, in case we're called from RSF
 		checkRunData(request);
@@ -1949,7 +1951,7 @@ public class ResourcesHelperAction extends VelocityPortletPaneledAction
 			} else {
 				JetspeedRunData rundata = (JetspeedRunData) request.getAttribute(ATTR_RUNDATA);
 				if (checkCSRFToken(request,rundata,action)) {
-					doDragDropUpload(request, response, fullPath);
+					doDragDropUpload(request, response, fullPath, hidden);
 				}
 			}
 		}
@@ -1982,7 +1984,7 @@ public class ResourcesHelperAction extends VelocityPortletPaneledAction
 	}
 
 
-	private synchronized void doDragDropUpload(HttpServletRequest request, HttpServletResponse response, String fullPath)
+	private synchronized void doDragDropUpload(HttpServletRequest request, HttpServletResponse response, String fullPath, boolean hidden)
 	{
 		//Feel free to sent comments/warnings/advices to me at daniel.merino AT unavarra.es
 		
@@ -2070,9 +2072,11 @@ public class ResourcesHelperAction extends VelocityPortletPaneledAction
 					resourceProps.addProperty(ResourcePropertiesEdit.PROP_DISPLAY_NAME, uploadFileName);
 					resource.setContent(uploadFile.getInputStream());
 					resource.setContentType(contentType);
+					resource.setAvailability(hidden, null, null);
 					ContentHostingService.commitResource(resource, NotificationService.NOTI_NONE);
 					
 					if (collection != null){
+						collection.setAvailability(hidden, null, null);
 						ContentHostingService.commitCollection(collection);
 						logger.debug("Collection commited: "+collection.getId());
 					}
