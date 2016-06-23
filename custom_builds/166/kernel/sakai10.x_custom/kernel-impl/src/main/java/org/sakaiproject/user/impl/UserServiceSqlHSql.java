86e70cd64fc9490825185453251e07e7b3288398
Kenny Aragon <kenny@longsight.com>
2016-06-23 20:30:34 +0000

    Adding cloud-content to LearnersEdge build

---
+++ custom_builds/166/kernel/sakai10.x_custom/kernel-impl/src/main/java/org/sakaiproject/user/impl/UserServiceSqlHSql.java
@@ -0,0 +1,29 @@
+/**********************************************************************************
+ * $URL: https://svn.rsmart.com/svn/vendor/branches/sakai-kernel/rsmart-cle/kernel-impl/src/main/java/org/sakaiproject/user/impl/UserServiceSqlHSql.java $
+ * $Id: UserServiceSqlHSql.java 26198 2011-04-08 19:28:14Z jcrodriguez $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2007, 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.user.impl;
+
+/**
+ * methods for accessing user data in a hypersonic sql database.
+ */
+public class UserServiceSqlHSql extends UserServiceSqlDefault
+{
+}
