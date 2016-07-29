be98eedd1e345b552fa67ae242f2c19f3fafbc16
Earle Nietzel <enietzel@anisakai.com>
2016-07-29 15:30:18 +0000

    Update content-review with spring 4 tests

--- custom_builds/170/content-review/sakai-11.x/contentreview-impl/impl/src/test/org/sakaiproject/contentreview/logic/TurnitinImplTest.java
+++ custom_builds/170/content-review/sakai-11.x/contentreview-impl/impl/src/test/org/sakaiproject/contentreview/logic/TurnitinImplTest.java
@@ -2,30 +2,28 @@ package org.sakaiproject.contentreview.logic;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Test;
 import org.sakaiproject.contentreview.impl.turnitin.TurnitinReviewServiceImpl;
-import org.springframework.test.AbstractTransactionalSpringContextTests;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-public class TurnitinImplTest extends AbstractTransactionalSpringContextTests {
+@ContextConfiguration({"/hibernate-test.xml", "/spring-hibernate.xml"})
+public class TurnitinImplTest extends AbstractTransactionalJUnit4SpringContextTests {
 	private static final Log log = LogFactory.getLog(TurnitinImplTest.class);
 	
-	protected String[] getConfigLocations() {
-	      // point to the needed spring config files, must be on the classpath
-	      // (add component/src/webapp/WEB-INF to the build path in Eclipse),
-	      // they also need to be referenced in the maven file
-	      return new String[] {"hibernate-test.xml", "spring-hibernate.xml"};
-	   }
 
-	
+	@Test
 	public void testFileEscape() {
 		TurnitinReviewServiceImpl tiiService = new TurnitinReviewServiceImpl();
 		String someEscaping = tiiService.escapeFileName("Practical%203.docx", "contentId");
-		assertEquals("Practical_3.docx", someEscaping);
+		Assert.assertEquals("Practical_3.docx", someEscaping);
 		
 		someEscaping = tiiService.escapeFileName("Practical%203%.docx", "contentId");
-		assertEquals("contentId", someEscaping);
+		Assert.assertEquals("contentId", someEscaping);
 		
 		someEscaping = tiiService.escapeFileName("Practical3.docx", "contentId");
-		assertEquals("Practical3.docx", someEscaping);
+		Assert.assertEquals("Practical3.docx", someEscaping);
 		
 		
 	}
